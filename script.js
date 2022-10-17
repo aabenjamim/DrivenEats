5884de0fdd321726e0e1dff30b81e02dabd4b615
aabenjamim <alanebenjamimdossantos@gmail.com>
2022-10-17 13:55:32 -0300

    Versão final sem o bônus

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 let comida;
 let bebida;
 let sobremesa;
+let valor1;
+let valor2;
+let valor3;
 
 
 function selecionarItem1(itemClicado){
@@ -17,6 +20,12 @@ function selecionarItem1(itemClicado){
    const prato = document.querySelector('.comidas .selecionada>h1')
    
    comida = prato.innerHTML
+
+   const preco = document.querySelector('.comidas .selecionada .preco-item')
+
+   valor1 = (Number(preco.innerHTML.replace(',', '.')))
+
+   console.log(valor1)
 
    liberarBotao()
 }
@@ -36,6 +45,12 @@ function selecionarItem2(itemClicado){
    
     bebida = refri.innerHTML
 
+    const preco = document.querySelector('.bebidas .selecionada .preco-item')
+
+    valor2 = (Number(preco.innerHTML.replace(',', '.')))
+
+    console.log(valor2)
+
     liberarBotao()
  }
 
@@ -53,6 +68,12 @@ function selecionarItem2(itemClicado){
    
     sobremesa = doce.innerHTML
 
+    const preco = document.querySelector('.sobremesas .selecionada .preco-item')
+
+    valor3 = (Number(preco.innerHTML.replace(',', '.')))
+
+    console.log(valor3)
+
     liberarBotao()
  }
 
@@ -63,4 +84,20 @@ function selecionarItem2(itemClicado){
     if ( comida !== undefined && bebida!== undefined && sobremesa !== undefined){
         botaoPedido.classList.remove('escondido')
     }
+ }
+
+ function fazerPedido(){
+    
+    const total = valor1 + valor2 + valor3 
+
+    const totalformatado = (total.toFixed(2).replace('.', ','))
+
+    const mensagem = 'Olá, gostaria de fazer o pedido:\n- Prato: ' 
+    + comida + '\n- Bebida: ' + bebida + '\n- Sobremesa: ' + sobremesa + 
+    '\nTotal: R$ '+ totalformatado
+
+    const mensagemzap = encodeURIComponent(mensagem)
+
+    window.open('https://wa.me/5588900000000?text=' + mensagemzap)
+
  }
